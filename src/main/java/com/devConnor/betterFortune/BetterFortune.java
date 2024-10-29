b01bf9f3944bd293417e5131c6bc049ac212277f
Connor McKenzie <connormckenzie@mysite.local>
2024-10-29 19:47:45 +0000

    Initial commit

---
+++ src/main/java/com/devConnor/betterFortune/BetterFortune.java
@@ -0,0 +1,16 @@
+package com.devConnor.betterFortune;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterFortune extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
